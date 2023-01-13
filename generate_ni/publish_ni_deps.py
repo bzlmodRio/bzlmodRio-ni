15363b270f9fb1bb3c88e51b07694e632f8bf41f
PJ Reiniger <pj.reiniger@gmail.com>
2023-01-12 23:44:20 -0500

    Generation

---
+++ generate_ni/publish_ni_deps.py
@@ -0,0 +1,26 @@
+
+import os
+from get_ni_dependencies import get_ni_dependencies
+from bazelrio_gentool.generate_json import generate_json
+
+def main():
+    SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+    registry_location = os.path.join(SCRIPT_DIR, "..", '..', "bazel-central-registry")
+    print(registry_location)
+    print(os.path.exists(registry_location))
+
+    group = get_ni_dependencies()
+    
+    module_template = os.path.join(SCRIPT_DIR, "module_config.jinja2")
+    module_json_template = os.path.join(SCRIPT_DIR, "module_config.json.jinja2")
+    print(module_json_template)
+
+    os.chdir(SCRIPT_DIR)
+    generate_json(registry_location, group, module_json_template, module_template)
+
+
+if __name__ == "__main__":
+    """
+    bazel run @bzlmodrio-ni//generate_ni:publish_ni_deps --enable_bzlmod
+    """
+    main()
